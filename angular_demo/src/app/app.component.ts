b36559a27ddaf53f784dd998c88c54bc1f353b51
Jon Prell <JTP709@gmail.com>
2022-08-25 09:28:03 -0600

    update some headers to consistency

--- angular_demo/src/app/app.component.ts
+++ angular_demo/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'Todo App';
+  title = 'TODO App';
   todoList: string[] = [];
 
   ngOnInit(): void {
